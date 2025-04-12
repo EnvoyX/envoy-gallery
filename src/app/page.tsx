7ce8cc1967ea4b1f344f31af769d8d30711218c9
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-04-12 12:37:07 +0700

    chore : displayed mock data from uploadthing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://vpj0o8x22x.ufs.sh/f/r2ogEo82HpIZFkvfd74DxIQyaG9AMln8XoZbY2L7NB4mgicP",
+  "https://vpj0o8x22x.ufs.sh/f/r2ogEo82HpIZPIXELxWevSgHGi3UcWZldQwanz1KT8YL4Fso",
+  "https://vpj0o8x22x.ufs.sh/f/r2ogEo82HpIZgV1Jm4KTutOjX29Zo3Da1Kepx7bWyF8skn06",
+  "https://vpj0o8x22x.ufs.sh/f/r2ogEo82HpIZDG8gdimoUqKDefTchMJmi3npa7vQXVGPB1wR",
+];
+
+const mockImages = mockUrls.map((url, idx) => ({
+  id: idx + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (Envoy Gallery)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="image mockup" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
