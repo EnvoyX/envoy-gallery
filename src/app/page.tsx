4199beda960cf661aab21faf9bd39ab29bccf2da
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-04-12 18:22:41 +0700

    fix : fix key error on mockImages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,8 @@ export default function HomePage() {
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {[...mockImages, ...mockImages, ...mockImages].map((image, idx) => (
+          <div key={idx} className="w-48">
             <img src={image.url} alt="image mockup" />
           </div>
         ))}
